dbb84a31dbe43ee344907ad587ee33fc3b76fd57
fmitei <faith.mitei@trustgro.com>
2023-05-21 10:21:34 +0300

    completed lab

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] }
+ 
+  validate :clickbaity_title
+
+      def clickbaity_title
+          clickbait_phrases = ["Won't Believe", "Secret", "Top", "Guess"]
+           if title.nil? || clickbait_phrases.none? { |phrase| title.include?(phrase) }
+            errors.add(:title, "must be sufficiently clickbait-y")
+        end
+      end
 end
